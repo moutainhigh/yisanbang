b04b46a5731ded9f63fbed066dc160ab29d4b8ec
smietao <709327956@qq.com>
2020-01-17 14:24:44 +0800

    初始项目提交

---
+++ src/main/java/com/vtmer/yisanbang/YisanbangApplication.java
@@ -0,0 +1,13 @@
+package com.vtmer.yisanbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YisanbangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YisanbangApplication.class, args);
+    }
+
+}
