45193c5067ebe93178a56baef3e8364fc3b43f7d
YaYaHuang <570911275@qq.com>
2020-02-01 11:46:29 +0800

    商品细节表重新生成2

---
+++ src/main/java/com/vtmer/yisanbang/domain/GoodsDetail.java
@@ -0,0 +1,65 @@
+package com.vtmer.yisanbang.domain;
+
+import java.util.Date;
+
+public class GoodsDetail {
+    private Integer id;
+
+    private Integer goodsId;
+
+    private String pirtucePath;
+
+    private Integer showOrder;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getPirtucePath() {
+        return pirtucePath;
+    }
+
+    public void setPirtucePath(String pirtucePath) {
+        this.pirtucePath = pirtucePath == null ? null : pirtucePath.trim();
+    }
+
+    public Integer getShowOrder() {
+        return showOrder;
+    }
+
+    public void setShowOrder(Integer showOrder) {
+        this.showOrder = showOrder;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
