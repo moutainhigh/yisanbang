7efbd64a7b8f3aba0baaeb31159f25d69c5cb72e
YaYaHuang <570911275@qq.com>
2020-02-09 04:15:09 +0800

    添加了库存的判断，增加，减少。查找显示部分于商品与套装代码内。

--- src/main/java/com/vtmer/yisanbang/vo/IncomeVo.java
+++ src/main/java/com/vtmer/yisanbang/vo/IncomeVo.java
@@ -13,4 +13,28 @@ public class IncomeVo {
     private Integer totalAmount;
 
     private List<Income> incomeList;
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Integer totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public List<Income> getIncomeList() {
+        return incomeList;
+    }
+
+    public void setIncomeList(List<Income> incomeList) {
+        this.incomeList = incomeList;
+    }
 }
