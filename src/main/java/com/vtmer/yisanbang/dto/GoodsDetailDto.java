3fd9f96104205def98bdcfb8b78651206d045dab
YaYaHuang <570911275@qq.com>
2020-02-11 05:20:59 +0800

    商品详情接口——测试完成     添加数据检验

--- src/main/java/com/vtmer/yisanbang/dto/GoodsDetailDto.java
+++ src/main/java/com/vtmer/yisanbang/dto/GoodsDetailDto.java
@@ -1,19 +1,28 @@
 package com.vtmer.yisanbang.dto;
 
+import com.vtmer.yisanbang.common.validGroup.Delete;
+import com.vtmer.yisanbang.common.validGroup.Update;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @ApiModel(value = "goodsDetail对象", description = "商品对象goodsDetail")
 public class GoodsDetailDto {
+    @NotNull(groups = {Update.class, Delete.class}, message = "商品详情id不可为空")
     @ApiModelProperty(value = "商品详情id", example = "1")
     private Integer id;
 
+    @NotNull(message = "商品id不可为空")
     @ApiModelProperty(value = "商品id", example = "1")
     private Integer goodsId;
 
+    @NotBlank(message = "商品详情图片不可为空")
     @ApiModelProperty(value = "商品详情图片", example = "goodsDetail/****")
     private String pirtucePath;
 
+    @NotNull(message = "显示顺序不可为空")
     @ApiModelProperty(value = "显示顺序", example = "2")
     private Integer showOrder;
 
