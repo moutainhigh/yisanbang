8afb21aaa3117e23b4fe991706d3e3d54c0dbf62
smietao <709327956@qq.com>
2020-02-12 21:35:56 +0800

    开启mysql对应驼峰命名法 注意mysql账号密码重新修改 添加服务器Swagger资源放行

--- src/main/java/com/vtmer/yisanbang/common/config/CorsConfig.java
+++ src/main/java/com/vtmer/yisanbang/common/config/CorsConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-public class CorsConfig {
+public class CorsConfig extends WebMvcConfigurationSupport {
+
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("*"); //允许任何域名
@@ -20,4 +23,19 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", buildConfig()); //注册
         return new CorsFilter(source);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOrigins("*")
+                // 是否允许证书 不再默认开启
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+
 }
