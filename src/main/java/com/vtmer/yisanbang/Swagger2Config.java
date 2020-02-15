f3dca6df5bd92ba237ec9e81dbbff98dd028e0bf
smietao <709327956@qq.com>
2020-02-15 09:03:45 +0800

    Swagger ui全局添加token认证

--- src/main/java/com/vtmer/yisanbang/Swagger2Config.java
+++ src/main/java/com/vtmer/yisanbang/Swagger2Config.java
@@ -1,39 +1,57 @@
 package com.vtmer.yisanbang;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
 
+    /**实现点：业务系统的token认证机制**/
+    private List<Parameter> getTokenPar(){
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        tokenPar.name("accessToken")
+                .description("鉴权token")
+                .modelRef(new ModelRef("string")).parameterType("header").required(true).build();
+        pars.add(tokenPar.build());
+        return pars;
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .host("119.3.254.171:8089")
                 .apiInfo(getApiInfo())
                 .select()
-                //.apis(RequestHandlerSelectors.basePackage("com.vtmer.yisanbang.controller")) // 过滤的接口
-                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .apis(RequestHandlerSelectors.basePackage("com.vtmer.yisanbang.controller")) // 过滤的接口
+                //.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 //.apis(RequestHandlerSelectors.withMethodAnnotation(Api.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(getTokenPar());
     }
 
     private ApiInfo getApiInfo() {
         Contact contact = new Contact("name","url","email");
         return new ApiInfoBuilder()
-                .title("标题：【衣仨邦】微信小程序")
-                .description("描述：项目描述：服饰电商小程序")
-                .version("项目版本1.0")
+                .title("【衣仨邦】微信小程序")
+                .description("项目描述：服饰电商小程序")
+                .version("版本1.1")
                 .contact(contact)
                 .build();
     }
