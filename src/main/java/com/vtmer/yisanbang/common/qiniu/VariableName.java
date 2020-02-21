52658e1884187eadb6289132ab85e3ce1b733eb5
eeatem <eeatem@qq.com>
2020-02-21 17:50:32 +0800

    修改七牛云配置信息为甲方存储空间及域名

--- src/main/java/com/vtmer/yisanbang/common/qiniu/VariableName.java
+++ src/main/java/com/vtmer/yisanbang/common/qiniu/VariableName.java
@@ -2,11 +2,11 @@ package com.vtmer.yisanbang.common.qiniu;
 
 public class VariableName {
     // 七牛AK
-    public static final String accessKey = "3q3Owi1RR6unxQZJvH9PcfAKY7mukywOdEVyTAvD";
+    public static final String accessKey = "4tKLjmmqlHx9IcQ-69lY6yNf0lP8SZZnatOhuLm1";
     // 七牛SK
-    public static final String secretKey = "lTMii9NXaR03m6UgiHVHeapJ9_5d6vp23wymA-k6";
+    public static final String secretKey = "X_WXqGezWrTOWT2tU9cqLNFfJMy6heKCBlDnvof5";
     // 七牛存储空间名
-    public static final String bucket = "eeatem";
+    public static final String bucket = "yisanbang";
     // 七牛默认域名
-    public static final String domain = "http://img.eeatem.top/";
+    public static final String domain = "http://img.yisabang.com/";
 }
