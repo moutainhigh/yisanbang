d34b425ebe9f972205d6bd89743393fad84fffdf
70932 <709327956@qq.com>
2020-02-28 19:17:39 +0800

    修改日志aop切面

--- src/main/java/com/vtmer/yisanbang/common/aop/SystemLogAspect.java
+++ src/main/java/com/vtmer/yisanbang/common/aop/SystemLogAspect.java
@@ -4,9 +4,11 @@ import com.vtmer.yisanbang.common.annotation.RequestLog;
 import com.vtmer.yisanbang.common.util.JwtUtil;
 import com.vtmer.yisanbang.domain.User;
 import com.vtmer.yisanbang.shiro.JwtFilter;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
@@ -38,8 +40,8 @@ public class SystemLogAspect {
     /**
      * 配置controller环绕通知,使用在方法aspect()上注册的切入点
      */
-    @Around("controllerAspect()")
-    public Object aroundMethod(ProceedingJoinPoint point) throws Throwable {
+    @Before("controllerAspect()")
+    public void aroundMethod(JoinPoint point) throws Throwable {
         if (logger.isDebugEnabled()) {
             logger.info(">>>>>>>>>>>>>>>进入日志切面<<<<<<<<<<<<<<<<");
         }
@@ -57,27 +59,13 @@ public class SystemLogAspect {
         //这里是从token中获取用户信息，打印当前的访问用户
         String token = request.getHeader(JwtUtil.TOKEN_HEADER);
         if (token != null) {
-            User user = JwtFilter.getLoginUser();
-            logger.info("Current UserId is:[{}]",user.getId());
+            Integer userIdByToken = JwtUtil.getUserIdByToken(token);
+            logger.info("Current UserId is:[{}]",userIdByToken);
         }
         if (Arrays.toString(point.getArgs()).equals("")) {
             //打印请求参数，如果需要打印其他的信息可以到request中去拿
             logger.info("RequestParam:{}", Arrays.toString(point.getArgs()));
         }
-        // 返回通知(操作成功:1,操作失败:2)
-        int status = 0;
-        try {
-            point.proceed();
-            // 设置请求结果
-            // 返回通知(操作成功:1,操作失败:2)
-            status = 1;
-        } catch (Throwable e) {
-            status = 2;
-        } finally {
-            // 后置通知
-            logger.info("后置通知>>>>>>>>>>>>>>>操作模块：" + module + ",操作方法：" + methodTarget + "，操作行为：" + desc + ",操作结果：" + status + "!(操作成功:1,操作失败:2)<<<<<<<<<<<<<<<<");
-        }
-        return null;
     }
 
     /**
