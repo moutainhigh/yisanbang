a8785cdf626000a7202ed1f764d8a062ebeafa4f
eeatem <eeatem@qq.com>
2020-03-11 11:07:34 +0800

    添加首页广告接口文件

---
+++ src/test/java/com/vtmer/yisanbang/Test1.java
@@ -0,0 +1,13 @@
+package com.vtmer.yisanbang;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1() {
+        Integer num = null;
+        System.out.println(num);
+    }
+
+}
